c968dcf548420e95979e197ab3d40345c140de28
yunahamazawa <yunahamazawa821@gmail.com>
2024-11-26 20:54:26 +0900

    演習⑤

--- src/Person.java
+++ src/Person.java
@@ -8,12 +8,12 @@ public class Person {
 		this.age=age;
 	}
 	
-	public String getName(){
-		return this.name;
+	public int getAge(){
+		return this.age;
 	}
 	
-	public void setName(String name){
-		this.name = name;
+	public void setAge(int age){
+		this.age = age;
 	}
 
 }
